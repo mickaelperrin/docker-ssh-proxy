cc6c6a602e51f70b6cec65169c569a6a5e4c6254
Mickaël PERRIN <dev@mickaelperrin.fr>
2018-11-06 12:36:28 +0100

    [FEATURE] All in one docker composer file example

--- bundled/reload-on-change.sh
+++ bundled/reload-on-change.sh
@@ -8,6 +8,10 @@ while [ -z "${PID}" ]; do
   sleep 1
 done
 
+# Ensure that the configuration is properly generated
+# if file has been generated before the watcher
+/generateConfig.sh
+
 # Restart process if config file has changed
 # ------------------------------------------
 while inotifywait -q -e create,delete,modify,attrib /etc/sshpiper/docker.generated.conf; do
